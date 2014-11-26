595077a6708e03a545189ce78f423354a4f89d01
jableader <jacobdunk@gmail.com>
2014-11-26 19:36:56 +1100

    More work done

--- GolfServer/views.py
+++ GolfServer/views.py
@@ -1,9 +1,36 @@
-from django.shortcuts import RequestContext, render_to_response
+from django.contrib.auth import authenticate, login, logout as logoutUser
+from django.shortcuts import RequestContext, render, redirect
 from django.views import generic
 from models import User
 
+HOME = 'home'
+
+def getNextUrl(request):
+    if 'next' in request.GET: return request.GET['next']
+    return HOME
+
+def profile(request, user_id):
+    i = 3/0 #How do I throw exceptions....
 
 # Create your views here.
-def home(response):
-    context = RequestContext(response)
-    return render_to_response('home.html', {}, context)
+def home(request):
+    return render(request, 'home.html', {})
+
+def login_form(request):
+    if request.user.is_authenticated():
+        return redirect(getNextUrl(request))
+    else:
+        hasTriedBefore = False
+        if 'username' in request.POST and 'password' in request.POST:
+            user = authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user is not None:
+                login(request, user)
+                return redirect(getNextUrl(request))
+            else:
+                hasTriedBefore = True
+
+        return render(request, 'login_form.html', {'hasTriedBefore': hasTriedBefore, 'next': getNextUrl(request)})
+
+def logout(request):
+    logoutUser(request)
+    return redirect(HOME)
--- golf/urls.py
+++ golf/urls.py
@@ -1,11 +1,17 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from GolfServer import views as golfviews
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'golf.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', golfviews.home),
+    url(r'^$', golfviews.home, name='home'),
+    url(r'^login$', golfviews.login_form, name='login'),
+    url(r'^logout$', golfviews.logout, name='logout'),
+    url(r'users/profile/$', golfviews.profile, name='profile'),
+
+
     url(r'^admin/', include(admin.site.urls)),
 )

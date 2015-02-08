62d6fccb090de3df4fb0d873845536b85994825a
jableader <jacobdunk@gmail.com>
2015-02-09 08:57:01 +1100

    Colored syntax **working**

--- GolfServer/templatetags/codify.py
+++ GolfServer/templatetags/codify.py
@@ -8,14 +8,10 @@ from django.utils.safestring import mark_safe
 from keyword import kwlist
 
 import re
-keywords = re.compile('\\b(' + '|'.join(kwlist) + ')\\b')
-comments = re.compile('#.*\n')
-strings = re.compile('(\'|").*\\1')
-digit = re.compile('\d+')
 word_regex = re.compile(r"\w+")
 
 def span(klass, contents):
-    return '<span class="%s">%s</span>' % (klass, contents)
+    return '<span class="%s">%s</span>' % (klass, escape(contents))
 
 def index_of(f, collection, i=0):
     while i < len(collection) and not f(collection[i], i):
@@ -23,23 +19,27 @@ def index_of(f, collection, i=0):
     return i
 
 def format(s):
+    index = lambda f, i=0: index_of(f, s, i)
+    count = lambda desired, i=0: index_of(lambda actual, i: actual != desired, s)
+
     if s == '':
         return ''
     if s[0] in ('\r', '\n'):
         return format(s[1:])
     elif s[0] == '\t':
-        return '&nbsp'*4 + format(s[1:])
+        cnt = count('\t')
+        return '&nbsp'*4 * cnt + format(s[cnt:])
     elif s[0] == ' ':
-        how_many = index_of(lambda char, i: char != ' ', s)
-        return '&nbsp'*how_many + format(s[how_many:])
+        cnt = count(' ')
+        return '&nbsp'*cnt+ format(s[cnt:])
     elif s[0] in ('"', "'"):
-        s_end = index_of(lambda char, i: char == s[0] and s[i-1] != '\\', s, 1)
+        s_end = index(lambda c, i: c == s[0] and s[i-1] != '\\', 1) + 1
         return span('string', s[:s_end]) + format(s[s_end:])
     elif s[0].isdigit():
-        end = index_of(lambda c, i: not c.isdigit(), s)
+        end = index(lambda c, i: not c.isdigit())
         return span('digit', s[:end]) + format(s[end:])
     elif s[0] == '#':
-        end = 1 + s.index('\n')
+        end = index(lambda c, i: c == '\n')
         return span('comment', s[:end]) + format(s[end:])
     else:
         match = word_regex.match(s)
@@ -49,9 +49,7 @@ def format(s):
             else:
                 return match.group() + format(s[match.end():])
         else:
-            return s[0] + format(s[1:])
-
-span_base = ''
+            return escape(s[0]) + format(s[1:])
 
 @register.filter
 def as_code(value):
